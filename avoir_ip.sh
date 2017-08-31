c05d13d74b4b7efefe6ebf75f60f26482a9d8507
Eric Dubreuil <eric-externe.dubreuil@edelia.fr>
2017-08-31 15:30:37 +0200

    remplacement de la variable interface par vpc_id

--- avoir_ip.sh
+++ avoir_ip.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/bash -xv
 
 INTERFACE=$(curl --silent http://169.254.169.254/latest/meta-data/network/interfaces/macs/)
 VPC_ID=$(curl --silent http://169.254.169.254/latest/meta-data/network/interfaces/macs/${INTERFACE}/vpc-id)
 
-aws ec2 describe-instances --filters "Name=vpc-id, Values="${INTERFACE}"" --query "Reservations[*].Instances[*].PrivateIpAddress" --output text
+aws ec2 describe-instances --filters "Name=vpc-id, Values="${VPC_ID}"" --query "Reservations[*].Instances[*].PrivateIpAddress" --output text > /home/ec2-user/text
