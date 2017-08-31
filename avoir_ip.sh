b8fbcc590536a59669b969755f3bbfc35a862ea0
Eric Dubreuil <eric-externe.dubreuil@edelia.fr>
2017-08-31 16:14:23 +0200

    ecrasement du fichier hosts d'ansible

--- avoir_ip.sh
+++ avoir_ip.sh
@@ -3,5 +3,7 @@
 INTERFACE=$(curl --silent http://169.254.169.254/latest/meta-data/network/interfaces/macs/)
 VPC_ID=$(curl --silent http://169.254.169.254/latest/meta-data/network/interfaces/macs/${INTERFACE}/vpc-id)
 
-aws ec2 describe-instances --filters "Name=vpc-id, Values="${VPC_ID}"" --query "Reservations[*].Instances[*].PrivateIpAddress" --output text > /etc/ansible/hosts
+aws ec2 describe-instances --filters "Name=vpc-id, Values="${VPC_ID}"" --query "Reservations[*].Instances[*].PrivateIpAddress" --output text > /home/ec2-user/ed/hosts
+sudo mv /etc/ansible/hosts{,.bak}
+sudo cp /home/ec2-user/ed/hosts /etc/ansible/hosts
 
