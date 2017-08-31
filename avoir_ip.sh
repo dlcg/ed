7aacf74864aff9623d6fad42caba4541691bcf55
Eric Dubreuil <eric-externe.dubreuil@edelia.fr>
2017-08-31 15:50:52 +0200

    redirection dans le fichier hosts d ansible

--- avoir_ip.sh
+++ avoir_ip.sh
@@ -3,4 +3,5 @@
 INTERFACE=$(curl --silent http://169.254.169.254/latest/meta-data/network/interfaces/macs/)
 VPC_ID=$(curl --silent http://169.254.169.254/latest/meta-data/network/interfaces/macs/${INTERFACE}/vpc-id)
 
-aws ec2 describe-instances --filters "Name=vpc-id, Values="${VPC_ID}"" --query "Reservations[*].Instances[*].PrivateIpAddress" --output text > /home/ec2-user/text
+aws ec2 describe-instances --filters "Name=vpc-id, Values="${VPC_ID}"" --query "Reservations[*].Instances[*].PrivateIpAddress" --output text > /etc/ansible/hosts
+
