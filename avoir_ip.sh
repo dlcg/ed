8f522364393a0baab042a2e7f987f4d3b5e73e22
Eric Dubreuil <eric-externe.dubreuil@edelia.fr>
2017-09-01 14:34:06 +0200

    ajout de la creation de volume

--- avoir_ip.sh
+++ avoir_ip.sh
@@ -6,4 +6,4 @@ VPC_ID=$(curl --silent http://169.254.169.254/latest/meta-data/network/interface
 aws ec2 describe-instances --filters "Name=vpc-id, Values="${VPC_ID}"" --query "Reservations[*].Instances[*].PrivateIpAddress" --output text > /home/ec2-user/ed/hosts
 sudo mv /etc/ansible/hosts{,.bak}
 sudo cp /home/ec2-user/ed/hosts /etc/ansible/hosts
-
+sudo sed  -i '1i [Tout]' /etc/ansible/hosts
