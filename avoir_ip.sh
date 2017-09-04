8208fa6135971afc3b67199b0cb99a441555675e
Eric Dubreuil <eric-externe.dubreuil@edelia.fr>
2017-09-04 14:46:55 +0200

    Changement cassandra.yaml par volume.yaml

--- avoir_ip.sh
+++ avoir_ip.sh
@@ -9,4 +9,4 @@ sudo cp /home/ec2-user/ed/hosts /etc/ansible/hosts
 sudo sed -i '1i [Tout]' /etc/ansible/hosts
 sudo sed -i '62i host_key_checking = False' /etc/ansible/ansible.cfg
 sudo ansible-playbook /home/ec2-user/ed/yum.yaml
-sudo ansible-playbook /home/ec2-user/ed/cassandra.yaml
+sudo ansible-playbook /home/ec2-user/ed/volume.yaml
