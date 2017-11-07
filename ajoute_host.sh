afa8014242423a19f01b0387bb27ce35df03f180
Eric Dubreuil <eric-externe.dubreuil@edelia.fr>
2017-11-07 16:55:24 +0100

    shell

--- ajoute_host.sh
+++ ajoute_host.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 ANSIBLE="/etc/ansible/hosts"
-cd /home/eirik/aws
+REP="/appli/deploiement"
+
+cd ${REP}
+
 IP=$(/home/eirik/terraform/terraform output ip_publique_1)
 IP1=$(/home/eirik/terraform/terraform output ip_publique_2)
 IP2=$(/home/eirik/terraform/terraform output ip_prive_mysql)
@@ -19,6 +22,7 @@ echo "[cassandra]" >> ${ANSIBLE}
 echo "${IP3}" >> ${ANSIBLE}
 echo "[locahost]" >> ${ANSIBLE}
 echo "127.0.0.1" >> ${ANSIBLE}
-sed -i 's/Hostname.*/Hostname '${IP}'/' ../ed/ssh.cfg
-sed -i 's/remote.*/remote '${IP}' 443/' ../ed/client-common.txt
+
+sed -i 's/Hostname.*/Hostname '${IP}'/' ${REP}/ssh.cfg
+sed -i 's/remote.*/remote '${IP}' 443/' ${REP}/client-common.txt
 
